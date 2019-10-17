c89ed3a0b3ad8e666bb24cfd4552d5f170e00e92
Jimmy Chion <ajimmykid@gmail.com>
2019-10-16 23:24:24 -0400

    2016 archive

--- src/layout/util.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-export const Spacer = styled.div`
-	height: ${props=>props.height}px;
-	@media screen and (max-width: 767px) {
-		height: ${props=>props.xsHeight}px;
-	}
-`;
--- src/theme.js
+++
@@ -1,8 +0,0 @@
-const theme = {
-  fonts: {
-  	avant: "'itc-avant-garde-gothic-pro', Helvetica, Arial, sans-serif",
-  	p: "Georgia, Times New Roman, serif",
-  	helvetica: "'Helvetica Neue', Helvetica, Arial, sans-serif",
-  }
-}
-export default theme
