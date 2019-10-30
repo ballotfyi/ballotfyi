03c0589e6a2d986fccc9ece7aead5f823c51c978
Jimmy Chion <ajimmykid@gmail.com>
2019-10-29 21:52:03 -0400

    visual update

--- src/layout/BottomWarning.js
+++ src/layout/BottomWarning.js
@@ -3,24 +3,28 @@ import styled from 'styled-components'
 
 const BottomSticky = styled.div`
   position: fixed;
-  font-family: ${props=>props.theme.fonts.avant};
   font-size: 14px;
   bottom: 0;
+  left: 0;
+  width: 100%;
   background-color: #333;
   color: white;
   display: flex;
   justify-content: center;
   align-items: center;
-  height: 60px;
-  width: 100%;
+  min-height: 60px;
   z-index: 999;
+  padding: 10px 30px;
+  margin-left: auto;
+  margin-right: auto;
+  box-sizing: border-box;
+  text-align: center;
 `;
 
 const BottomWarning = () => {
   return(
     <BottomSticky>
-      You're viewing the 2018 guide. Here's how to&nbsp;<a href="https://www.ballot.fyi/">return to 2020</a>, when you're ready.
-
+      <span>You're viewing the 2018 guide. Here's how to <a href="https://www.ballot.fyi/">return to 2020</a>, when you're ready.</span>
     </BottomSticky>
   )
 }
