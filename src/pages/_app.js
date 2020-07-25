55b53b47a2c7e13452d1fc95ba0081fd051da67f
cjimmy <jimmychion@gmail.com>
2020-07-25 13:36:47 -0400

    window not defined issue

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,17 +1,18 @@
 import { useEffect } from 'react';
 import Router from 'next/router';
-import { pageview } from 'lib/gtag';
 
 const App = ({ Component, pageProps }) => {
   //-- track page views
   useEffect(() => {
-    const isProd = process.env.NODE_ENV === 'production'; // eslint-disable-line
-    const handleRouteChange = (url) => {
-      if (isProd) {
-        pageview(url);
-      }
-    };
-    Router.events.on('routeChangeComplete', handleRouteChange);
+    import('lib/gtag').then( gtag => {
+      const isProd = process.env.NODE_ENV === 'production'; // eslint-disable-line
+      const handleRouteChange = (url) => {
+        if (isProd) {
+          gtag.pageview(url);
+        }
+      };
+      Router.events.on('routeChangeComplete', handleRouteChange);
+    })
     return () => {
       Router.events.off('routeChangeComplete', handleRouteChange);
     };
