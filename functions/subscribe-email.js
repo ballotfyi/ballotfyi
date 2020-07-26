148085a88a9940a6898b5f171bff9171ee320438
cjimmy <jimmychion@gmail.com>
2020-07-25 22:52:05 -0400

    removelocalhost

--- functions/subscribe-email.js
+++ functions/subscribe-email.js
@@ -1,6 +1,6 @@
 const functions = require('firebase-functions');
 const cors = require('cors')({
-  origin: [/\.ballot\.fyi$/, 'http://localhost:3000'],
+  origin: [/\.ballot\.fyi$/],
   methods: ['POST']
 });
 const axios = require('axios');
