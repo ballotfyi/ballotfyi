ad3f2786560bb1c5fd5a5fb47ec100c4c9ffbbda
cjimmy <jimmychion@gmail.com>
2020-07-31 00:07:38 -0400

    fixing font-loading script

--- src/components/Fonts/index.js
+++ src/components/Fonts/index.js
@@ -14,28 +14,32 @@ const AmpFonts = () => (
   ></amp-font>
 );
 
+const fontload = `
+  (function(){
+    if("fonts" in document){
+      if(sessionStorage.fontsLoaded) {
+        document.documentElement.className+="fonts-loaded";
+        return;
+      }
+      document.fonts.load("1em InterPre").then(function() {
+        document.documentElement.className+=" fonts-loading";
+        Promise.all([
+          document.fonts.load("500 1em Inter"),
+          document.fonts.load("italic 500 1em Inter")
+        ]).then(function(){
+          document.documentElement.className+=" fonts-loaded";
+          sessionStorage.fontsLoaded=true;
+        });
+      });
+    }
+  })();
+`
 const NonAmpFonts = () => (
   <Head>
-    <script>{`
-      (function(){
-        if("fonts" in document){
-          if(sessionStorage.fontsLoaded) {
-            document.documentElement.className+="fonts-loaded";
-            return;
-          }
-          document.fonts.load("1em InterPre").then(function() {
-            document.documentElement.className+=" fonts-loading";
-            Promise.all([
-              document.fonts.load("500 1em Inter"),
-              document.fonts.load("italic 500 1em Inter")
-            ]).then(function(){
-              document.documentElement.className+=" fonts-loaded";
-              sessionStorage.fontsLoaded=true;
-            });
-          });
-        }
-      })();
-    `}</script>
+    <script
+      type="text/javascript"
+      dangerouslySetInnerHTML={{ __html: fontload }}
+    ></script>
   </Head>
 );
 
