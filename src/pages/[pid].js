8f3cd8ad19a9719bf5033c1444c8c63d2693e04e
cjimmy <jimmychion@gmail.com>
2020-10-12 17:21:54 -0400

    revert that

--- src/pages/[pid].js
+++ src/pages/[pid].js
@@ -84,7 +84,7 @@ export async function getStaticPaths() {
       }));
       return {
         paths: paths,
-        fallback: true,
+        fallback: false,
       };
     } else {
       return defaultObj;
