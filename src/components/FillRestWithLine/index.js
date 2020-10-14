0629e416b2abfcde1a4dc1eb61c6621667696684
cjimmy <jimmychion@gmail.com>
2020-10-14 00:08:59 -0400

    not great but better than before

--- src/components/FillRestWithLine/index.js
+++ src/components/FillRestWithLine/index.js
@@ -1,75 +1,85 @@
-import { useState, useRef, useEffect, useCallback } from 'react';
-import PropTypes from 'prop-types';
+import { useState, useRef, useEffect, useLayoutEffect } from 'react';
+// import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { useAmp } from 'next/amp';
 
-const TextAndLine = (props) => {
-  const [lineWidth, setLineWidth] = useState(100);
-  const [isEmpty, setIsEmpty] = useState(false);
+const TextAndLineParent = (props) => {
+  const [showLine, setShowLine] = useState(0);
   const wrapperRef = useRef(null);
-  const innerContentRef = useRef(null);
+  const textRef = useRef(null);
 
-  const handleResize = useCallback(() => {
-    if (innerContentRef.current && wrapperRef.current) {
-      //-- wrapped in a set timeout so that it's delayed until after the render
-      setTimeout(() => {
-        const w = innerContentRef.current.getBoundingClientRect().width;
-        const totalW = wrapperRef.current.getBoundingClientRect().width;
-        const shouldBeEmpty = w === 0 || Math.abs(totalW - w) < 50;
-        const lineW = shouldBeEmpty ? 0 : totalW - w - 36; //-- totalWidth - textWidth - borderRadius*2 - marginLeft
-        setIsEmpty(shouldBeEmpty);
-        setLineWidth(lineW);
-      }, 0);
-    }
-  }, [wrapperRef, innerContentRef]);
+  // Wait until after client-side hydration to show
+  useEffect(() => {
+    setShowLine(1);
+  }, []);
 
   useEffect(() => {
+    function handleResize() {
+      setShowLine((prev) => prev + 1);
+    }
     window.addEventListener('resize', handleResize);
-    handleResize();
     return () => {
       window.removeEventListener('resize', handleResize);
     };
-  }, [handleResize]);
+  }, []);
 
   //-- escape for AMP
   const isAmp = useAmp();
   if (isAmp) return <span>{props.children}</span>;
 
-  const isReversed = props.align === 'right';
+  if (!showLine) {
+    return null;
+  }
+
   return (
-    <span ref={wrapperRef}>
-      <Container>
-        {isReversed && <Line isReversed={isReversed} lineWidth={lineWidth} isEmpty={isEmpty} />}
-        <span ref={innerContentRef}>{props.children}</span>
-        {!isReversed && <Line isReversed={isReversed} lineWidth={lineWidth} isEmpty={isEmpty} />}
-      </Container>
-    </span>
+    <Container ref={wrapperRef}>
+      <Text ref={textRef}>{props.children}</Text>
+      {showLine && <LineFill textRef={textRef} wrapperRef={wrapperRef} />}
+    </Container>
   );
 };
 
-TextAndLine.propTypes = {
-  align: PropTypes.string,
+const LineFill = (props) => {
+  const [lineWidth, setLineWidth] = useState(50);
+  const { textRef, wrapperRef } = props;
+  const textWidth = textRef && textRef.current ? textRef.current.getBoundingClientRect().width : 0;
+  const totalWidth =
+    wrapperRef && wrapperRef.current ? wrapperRef.current.getBoundingClientRect().width : 0;
+
+  useLayoutEffect(() => {
+    setTimeout(() => {
+      const shouldBeEmpty = textWidth === 0 || Math.abs(totalWidth - textWidth) < 50;
+      const lineW = shouldBeEmpty ? 0 : totalWidth - textWidth - 46; //-- totalWidth - textWidth - borderRadius*2 - marginLeft
+      setLineWidth(lineW);
+    }, 200);
+  }, [textWidth, totalWidth]);
+
+  return <Line lineWidth={lineWidth} />;
 };
 
-TextAndLine.defaultProps = {
-  align: 'left',
-};
-
-export default TextAndLine;
+export default TextAndLineParent;
 
 const Container = styled.div`
+  position: relative;
   width: 100%;
 `;
 
-const Line = styled.div`
+const Text = styled.div`
+  display: inline;
+`;
+
+const Line = styled.div.attrs((props) => ({
+  style: {
+    width: props.lineWidth,
+  },
+}))`
   display: inline-block;
   box-sizing: border-box;
   height: 0;
-  width: ${(props) => (props.lineWidth ? props.lineWidth + 'px' : 0)};
-  border: ${(props) => (props.isEmpty ? '0' : '3px solid #333')};
+  border: ${(props) => (props.lineWidth === 0 ? '0' : '3px solid #333')};
   border-radius: 3px;
-  margin-left: ${(props) => (props.isReversed ? 0 : props.isEmpty ? 0 : 20)}px;
-  margin-right: ${(props) => (props.isReversed ? (props.isEmpty ? 0 : 20) : 0)}px;
+  margin-left: ${(props) => (props.lineWidth === 0 ? 0 : 20)}px;
+  margin-right: ${(props) => (props.lineWidth === 0 ? 0 : 20)}px;
   margin-bottom: 6px;
   @media screen and (max-width: 767px) {
     border-radius: 2px;
